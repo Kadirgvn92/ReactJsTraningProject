06198ff085d908bb8080276b4641da129fad4d45
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-01 08:59:19 +0300

    UseState completed

--- 4.UseState/src/App.jsx
+++ 4.UseState/src/App.jsx
@@ -5,15 +5,26 @@ function App() {
   const [firstName, setFirstName] = useState('Kadir');
   const [lastName, setLastName] = useState('Güven')
   const [names, setNames] = useState(['Kadir', 'Enes', 'Ali', 'Veli'])
+  const [userInfo, setUserInfo] = useState({ userName: "Kguven", password: "1423" })
+  const [inputUserName, setInputUserName] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+  const [loginMessage, setLoginMessage] = useState('');
 
   const handleChange = () => {
     setFirstName('Haydar')
   }
 
+  const handleLogin = () => {
+    if (inputUserName === userInfo.userName && inputPassword === userInfo.password) {
+      setLoginMessage('Başarılı');
+    } else {
+      setLoginMessage('Başarısız');
+    }
+  };
   return (
     <div>
       {firstName} {lastName}
-
+      <br />
       <div><button onClick={() => { setFirstName('Osman') }}>İsmi Değiştir</button></div>
       <br />
       <div><button onClick={handleChange}>Başka İsim Değiştir</button></div>
@@ -23,6 +34,23 @@ function App() {
           names.map((name, index) => (
             <div key={index}>{name}</div>
           ))}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Kullanıcı Adı"
+          value={inputUserName}
+          onChange={(e) => setInputUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Şifre"
+          value={inputPassword}
+          onChange={(e) => setInputPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Giriş Yap</button>
+        <div>{loginMessage}</div>
       </div>
     </div>
 
