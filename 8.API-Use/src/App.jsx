e43075f0517d8dcf1c24a9eb96ed4f99c5513347
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-08 10:51:05 +0300

    Axios Get Method learned

--- 8.API-Use/src/App.jsx
+++ 8.API-Use/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react';
+
+
+const BASE_URL = "http://localhost:3006"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getAllUsers = async () => {
+    const response = await axios.get(BASE_URL + "/users");
+    console.log(response.data)
+  }
+  const getUserById = async (userID) => {
+    const response = await axios.get(`${BASE_URL}/users/${userID}`);
+    console.log(response.data)
+  }
+  useEffect(() => {
+    getUserById(2);
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
