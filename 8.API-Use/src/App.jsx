039c36e8fe255d0d7bdc38c6acdf6fba30529a62
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-09 14:55:10 +0300

    Sync methods process learned

--- 8.API-Use/src/App.jsx
+++ 8.API-Use/src/App.jsx
@@ -7,33 +7,54 @@ const BASE_URL = "http://localhost:3001"
 
 function App() {
 
-  const getAllUsers = async () => {
-    const response = await axios.get(BASE_URL + "/users");
-    console.log(response.data)
-  }
+  // const getAllUsers = async () => {
+  //   const response = await axios.get(BASE_URL + "/users");
+  //   console.log(response.data)
+  // }
+  // const getUserById = async (userID) => {
+  //   const response = await axios.get(`${BASE_URL}/users/${userID}`);
+  //   console.log(response.data)
+  // }
+
+  // const createUser = async (newUser) => {
+  //   const response = await axios.post(`${BASE_URL}/users`, newUser)
+  //   console.log(response)
+  // }
+
+  // const updateUser = async (userID, updatedUser) => {
+  //   const response = await axios.put(`${BASE_URL}/users/${userID}`, updatedUser)
+  //   console.log(response)
+
+  // }
+
+  // const deleteUserById = async (userID) => {
+  //   const response = await axios.delete(`${BASE_URL}/users/${userID}`)
+  //   console.log(response)
+  // }
+
+
   const getUserById = async (userID) => {
     const response = await axios.get(`${BASE_URL}/users/${userID}`);
-    console.log(response.data)
+    return response.data.postID;
   }
 
-  const createUser = async (newUser) => {
-    const response = await axios.post(`${BASE_URL}/users`, newUser)
+
+  const getPostByID = async (postID) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postID}`)
+    return response.data;
+  }
+
+  const getPost = async () => {
+    const postID = await getUserById(1);
+
+    const response = await getPostByID(postID);
+
     console.log(response)
   }
 
-  const updateUser = async (userID, updatedUser) => {
-    const response = await axios.put(`${BASE_URL}/users/${userID}`, updatedUser)
-    console.log(response)
-
-  }
 
   useEffect(() => {
-
-    const newUser = {
-      "username": "Bilal",
-      "password": "1234"
-    }
-    updateUser("1", newUser)
+    getPost()
   }, [])
 
   return (
